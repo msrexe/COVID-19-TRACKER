86adad3c35b537fc5e5012b902a1ad93b04213e6
msr.exe <mlheymen.ms@gmail.com>
2020-03-31 13:00:40 +0300

    Add files via upload
    Connection problem fixed

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import requests
 import re
 import urllib
 import time
+import datetime
+
+now = datetime.datetime.now()
 
 def connection(link):
-    response = requests.get(link)
-    if response.status_code == 200:
-        print("Api ile bağlantı sağlandı...")
-        print("\nVeriler karşı taraftan alındı...\n")
-        reg(response.content)
-        pass
+    key = 0
+    while not key == 1:
+        try:
+            response = requests.get(link)
+        except Exception:
+            print("\nİnternet bağlantısı sağlanamadı!!! Tekrar deneniyor....\n")
+            time.sleep(6)
+        else:
+            print("Api ile bağlantı sağlandı...")
+            print("\nVeriler karşı taraftan alındı...\n")
+            key = 1
+    reg(response.content)
 
 def reg(data):
     confirmedcases = re.findall(r"\"confirmed_cases\":(.*?),",data.decode("utf-8"))
@@ -21,14 +30,15 @@ def reg(data):
     recoveredpatients = re.findall(r"\"recovered_patients\":(.*?),",data.decode("utf-8"))    
     todaytests = re.findall(r"\"tests_done_today\":(.*?),",data.decode("utf-8"))
     print("**************************************************************************\n\n")
-    print("Tarih                == {}".format(date))
+    print("Tarih                  == {}".format(date))
     print("\nToplam Test Sayısı   == {}".format(totaltests))
     print("\nBugünkü Test Sayısı  == {}".format(todaytests))
     print("\nToplam Vaka Sayısı   == {}".format(confirmedcases))
     print("\nBugünkü Vaka Sayısı  == {}".format(cctoday))
     print("\nToplam Ölüm Sayısı   == {}".format(deaths))
     print("\nBugünkü Ölüm Sayısı  == {}".format(deathstoday))
-    print("\nİyileşen Vaka Sayısı == {}\n".format(recoveredpatients))
+    print("\nİyileşen Vaka Sayısı == {}".format(recoveredpatients))
+    print(f"\nSon Güncelleme      == {now}")
     print("\n\n**************************************************************************\n")
 
 print("\n\nTÜRKİYE GÜNLÜK KORONAVİRÜS TABLOSU\n")
